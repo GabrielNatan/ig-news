47bc038453db913ae3e7517e87b00650501b1926
Gabriel Natan <gabriel--natan@live.com>
2021-10-13 23:44:50 -0300

    style home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 
 import Head from "next/head"
+import Style from "./home.module.scss"
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>ig.News - Home</title>
       </Head>
-      <h1>
-        Hello <span> word</span>
-      </h1>
+      <main className={Style.contentContainer}>
+        <section className={Style.hero}>
+          <span> Hey, welcome</span>
+          <h1>News About the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9,98 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding"/>
+      </main>
     </>
   )
 }
