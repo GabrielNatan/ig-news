d4e255f76fa666c485af610892d66313f4be0a31
Gabriel Natan <gabriel--natan@live.com>
2021-10-13 22:52:24 -0300

    add component header

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,14 @@
+import Style from "./style.module.scss"
+export function Header() {
+    return (
+        <header className={Style.headerContainer}>
+            <div className={Style.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav >
+                    <a className={Style.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
