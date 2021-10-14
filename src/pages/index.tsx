03a6e8b29413d5ae2a163f58ab6f61c95d20e8d7
Gabriel Natan <gabriel--natan@live.com>
2021-10-13 22:53:34 -0300

    update global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
-import Styles from "../styles/home.module.scss"
+
+import Head from "next/head"
 export default function Home() {
   return (
-    <h1 className={Styles.title}>Hello <span> word</span></h1>
+    <>
+      <Head>
+        <title>ig.News - Home</title>
+      </Head>
+      <h1>
+        Hello <span> word</span>
+      </h1>
+    </>
   )
 }
