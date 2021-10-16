5ba1ea81dd20d327b4ed51fd1ec3e91d084a0b74
Gabriel Natan <gabriel--natan@live.com>
2021-10-16 14:19:10 -0300

    addd button home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 
+import {GetStaticProps} from "next"
 import Head from "next/head"
+import { SubscribeButton } from "../Components/SubscribeButton"
+import { stripe } from "../services/stripe"
 import Style from "./home.module.scss"
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}:HomeProps) {
   return (
     <>
       <Head>
@@ -14,12 +24,35 @@ export default function Home() {
           <h1>News About the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9,98 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding"/>
       </main>
     </>
   )
+}
+
+
+export const getStaticProps:GetStaticProps = async ()=>{
+  const price = await stripe.prices.retrieve('price_1JkWjJBT7RkQq2sXvugSpAaq',{
+    expand:['product']
+  })
+  
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style:'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
+  return{
+    props:{
+      product
+    },
+    revalidate: 60 * 60 * 24
+  }
 }
