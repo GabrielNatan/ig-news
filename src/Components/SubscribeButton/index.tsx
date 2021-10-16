ea0706e463fb2f7d72ab5326edd86a404224323d
Gabriel Natan <gabriel--natan@live.com>
2021-10-16 14:18:54 -0300

    add button

--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -1,20 +1,13 @@
 import Style from "./style.module.scss"
-import { FaGithub } from "react-icons/fa"
-import { FiX } from "react-icons/fi"
-export function SignInButton() {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
-        <button type="button" className={Style.signInButton}>
-            <FaGithub color="04d361" />
-            Jarvas maneiro
-            <FiX color="737388"  className={Style.closeIcon}/>
-        </button>
-    ) : (
 
-        <button type="button" className={Style.signInButton}>
-            <FaGithub color="eba417" />
-            Sign in with Github
-        </button>
+interface SubscribeButton {
+    priceId: string;
+}
 
-    )
+export function SubscribeButton({priceId}:SubscribeButton) {
+   return(
+       <button type="button" className={Style.subscribeButton}>
+           Subscribe now
+       </button>
+   )
 }
