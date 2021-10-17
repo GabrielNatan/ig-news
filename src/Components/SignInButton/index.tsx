b7569aec5d9834990d044d468a8cee64f925e095
Gabriel Natan <gabriel--natan@live.com>
2021-10-17 10:12:31 -0300

    login

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -1,9 +1,12 @@
 import Style from "./style.module.scss"
 import { FaGithub } from "react-icons/fa"
 import { FiX } from "react-icons/fi"
+import { signIn, useSession } from "next-auth/client"
+
 export function SignInButton() {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
+    const [session ] = useSession()
+   
+    return session ? (
         <button type="button" className={Style.signInButton}>
             <FaGithub color="04d361" />
             Jarvas maneiro
@@ -11,7 +14,11 @@ export function SignInButton() {
         </button>
     ) : (
 
-        <button type="button" className={Style.signInButton}>
+        <button 
+            type="button" 
+            className={Style.signInButton}
+            onClick={()=> signIn()}
+            >
             <FaGithub color="eba417" />
             Sign in with Github
         </button>
