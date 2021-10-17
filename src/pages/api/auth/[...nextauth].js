f8650850ab23b74cbd1b1cfdb087bd57bad7d7a4
Gabriel Natan <gabriel--natan@live.com>
2021-10-17 13:56:27 -0300

    connection db

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,8 @@
+import { query  as q} from "faunadb"
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +12,41 @@ export default NextAuth({
       clientSecret: '7bd71d3b6cc79f005f5893ba78a8e13c04f0f44c',
       scope:'read:user'
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn( user, account, profile ) {
+      const { email } = user
+     
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email2'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              {data: {email}}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email2'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+
+          
+        )
+        return true
+      } catch (error) {
+        return false
+      }
+     
+    },
+  }
 })
